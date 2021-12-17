285481b29522ee5d031b2fb1724aca16d9e3feaa
Bazzan <sebake01@gmail.com>
2021-12-17 13:57:57 +0100

    shoots staight

--- New folder (2)/src/ServerMain.cpp
+++ New folder (2)/src/ServerMain.cpp
@@ -16,129 +16,130 @@ bool gameStarted = false;
 void handleMessage(int userId, NetMessage msg)
 {
 	MessageType type = msg.read<MessageType>();
-	switch(type)
+	switch (type)
 	{
-		case MessageType::PlayerName:
+	case MessageType::PlayerName:
+	{
+		int playerId = msg.read<int>();
+		if (playerId != userId)
 		{
-			int playerId = msg.read<int>();
-			if (playerId != userId)
-			{
-				serverKickUser(userId);
-				break;
-			}
-
-			int nameLen = msg.read<unsigned char>();
-			if(nameLen > PLAYER_NAME_MAX)
-			{
-				serverKickUser(userId);
-				break;
-			}
-
-			Player* player = &players[userId];
-			msg.read(player->name, nameLen);
-
-			player->name[nameLen] = 0;
-
-			serverBroadcast(msg);
+			serverKickUser(userId);
 			break;
 		}
 
-		case MessageType::PlayerPosition:
+		int nameLen = msg.read<unsigned char>();
+		if (nameLen > PLAYER_NAME_MAX)
 		{
-			int playerId = msg.read<int>();
-			if(playerId != userId)
-			{
-				serverKickUser(userId);
-				break;
-			}
-
-			Player* player = &players[userId];
-			float newX = msg.read<float>();
-			float newY = msg.read<float>();
-			player->netReceivePosition(newX, newY);
-
-			serverBroadcast(msg);
+			serverKickUser(userId);
 			break;
 		}
 
-		case MessageType::PlayerInput:
+		Player* player = &players[userId];
+		msg.read(player->name, nameLen);
+
+		player->name[nameLen] = 0;
+
+		serverBroadcast(msg);
+		break;
+	}
+
+	case MessageType::PlayerPosition:
+	{
+		int playerId = msg.read<int>();
+		if (playerId != userId)
 		{
-			int playerId = msg.read<int>();
-			if (playerId != userId)
-			{
-				serverKickUser(userId);
-				break;
-			}
-
-			Player* player = &players[userId];
-			float newX = msg.read<float>();
-			float newY = msg.read<float>();
-			player->netReceivePosition(newX, newY);
-
-			player->inputX = msg.read<char>();
-			player->inputY = 0;
-			serverBroadcast(msg);
+			serverKickUser(userId);
 			break;
 		}
 
-		case MessageType::PlayerRequestFire:
+		Player* player = &players[userId];
+		float newX = msg.read<float>();
+		float newY = msg.read<float>();
+		player->netReceivePosition(newX, newY);
+
+		serverBroadcast(msg);
+		break;
+	}
+
+	case MessageType::PlayerInput:
+	{
+		int playerId = msg.read<int>();
+		if (playerId != userId)
 		{
-			if (!gameStarted)
-				break;
-
-			int projectileIndex = -1;
-			for(int i = 0; i < PROJECTILE_MAX; i++)
-			{
-				if (projectiles[i].alive)
-					continue;
-
-				projectileIndex = i;
-				break;
-			}
-
-			if(projectileIndex == -1)
-			{
-				engError("Ran out of projectiles");
-				break;
-			}
-
-			Player* player = &players[userId];
-
-			if (player->inputX == 0)
-				break;
-			if (engElapsedTime() - player->lastFireTime < playerFireCooldown)
-				break;
-
-			player->lastFireTime = engElapsedTime();
-			
-			if (player->teamId == 0)
-				projectiles[projectileIndex].spawn(userId, player->x, player->y, player->inputX, -1);
-			else
-				projectiles[projectileIndex].spawn(userId, player->x, player->y, player->inputX, 1);
-
-			NetMessage response;
-			response.write<MessageType>(MessageType::ProjectileSpawn);
-			response.write<int>(projectileIndex);
-			response.write<int>(userId);
-			response.write<float>(player->x);
-			response.write<float>(player->y);
-			response.write<char>(player->inputX);
-			if(player->id == 0)
-				response.write<char>(1);
-			else
-				response.write<char>(-1);
-
-
-			serverBroadcast(response);
-			response.free();
+			serverKickUser(userId);
 			break;
 		}
 
-		case MessageType::ProjectileHit:
+		Player* player = &players[userId];
+		float newX = msg.read<float>();
+		float newY = msg.read<float>();
+		player->netReceivePosition(newX, newY);
+
+		player->inputX = msg.read<char>();
+		//player->inputY = msg.read<char>();
+		serverBroadcast(msg);
+		break;
+	}
+
+	case MessageType::PlayerRequestFire:
+	{
+		if (!gameStarted)
+			break;
+
+		int projectileIndex = -1;
+		for (int i = 0; i < PROJECTILE_MAX; i++)
 		{
-			engPrint("A player was killed");
-			int playerId = msg.read<int>();
+			if (projectiles[i].alive)
+				continue;
+
+			projectileIndex = i;
+			break;
 		}
+
+		if (projectileIndex == -1)
+		{
+			engError("Ran out of projectiles");
+			break;
+		}
+
+		Player* player = &players[userId];
+
+		if (player->inputX == 0)
+			break;
+		if (engElapsedTime() - player->lastFireTime < playerFireCooldown)
+			break;
+
+		player->lastFireTime = engElapsedTime();
+
+		if (player->teamId == 0)
+			projectiles[projectileIndex].spawn(userId, player->x, player->y, 0, -1);
+		else
+			projectiles[projectileIndex].spawn(userId, player->x, player->y, 0, 1);
+
+		NetMessage response;
+		response.write<MessageType>(MessageType::ProjectileSpawn);
+		response.write<int>(projectileIndex);
+		response.write<int>(userId);
+		response.write<float>(player->x);
+		response.write<float>(player->y);
+		response.write<char>(0);
+		if (player->teamId == 0)
+			response.write<char>(-1);
+		else
+			response.write<char>(1);
+
+
+		serverBroadcast(response);
+		response.free();
+		break;
+	}
+
+	case MessageType::ProjectileHit:
+	{
+		engPrint("A player was killed");
+		int playerId = msg.read<int>();
+		serverKickUser(playerId);
+	}
 	}
 }
 
@@ -150,95 +151,95 @@ int WinMain(HINSTANCE, HINSTANCE, char*, int)
 	if (!serverStartup(666))
 		return 1;
 
-	while(engBeginFrame())
+	while (engBeginFrame())
 	{
 		NetEvent event;
-		while(netPollEvent(&event))
+		while (netPollEvent(&event))
 		{
-			switch(event.type)
+			switch (event.type)
 			{
-				case NetEventType::UserConnected:
+			case NetEventType::UserConnected:
+			{
+				if (gameStarted)
 				{
-					if (gameStarted)
-					{
-						serverKickUser(event.userId);
-						break;
-					}
-
-					engPrint("User %d connected", event.userId);
-					serverAcceptUser(event.userId);
-
-					for (int i = 0; i < PLAYER_MAX; i++)
-					{
-						if (!players[i].alive)
-							continue;
-
-						{
-							NetMessage spawnMsg;
-							spawnMsg.write<MessageType>(MessageType::PlayerSpawn);
-							spawnMsg.write<int>(i);
-							spawnMsg.write<float>(players[i].x);
-							spawnMsg.write<float>(players[i].y);
-
-							serverSendTo(spawnMsg, event.userId);
-							spawnMsg.free();
-						}
-						{
-							NetMessage nameMsg;
-							nameMsg.write<MessageType>(MessageType::PlayerName);
-							nameMsg.write<int>(i);
-							nameMsg.write<unsigned char>(strlen(players[i].name));
-							nameMsg.write(players[i].name, strlen(players[i].name));
-
-							serverSendTo(nameMsg, event.userId);
-							nameMsg.free();
-						}
-					}
-
-					{
-						Player* player = &players[event.userId];
-						player->spawn(event.userId, rand() % 800, rand() % 600);
-
-						NetMessage msg;
-						msg.write<MessageType>(MessageType::PlayerSpawn);
-						msg.write<int>(event.userId);
-						msg.write<float>(player->x);
-						msg.write<float>(player->y);
-
-						serverBroadcast(msg);
-						msg.free();
-					}
-
-					{
-						NetMessage msg;
-						msg.write<MessageType>(MessageType::PlayerPossess);
-						msg.write<int>(event.userId);
-
-						serverSendTo(msg, event.userId);
-						msg.free();
-					}
+					serverKickUser(event.userId);
 					break;
 				}
 
-				case NetEventType::UserDisconnected:
+				engPrint("User %d connected", event.userId);
+				serverAcceptUser(event.userId);
+
+				for (int i = 0; i < PLAYER_MAX; i++)
 				{
-					engPrint("User %d disconnected", event.userId);
-					players[event.userId].destroy();
+					if (!players[i].alive)
+						continue;
 
-					NetMessage destroyMsg;
-					destroyMsg.write<MessageType>(MessageType::PlayerDestroy);
-					destroyMsg.write<int>(event.userId);
+					{
+						NetMessage spawnMsg;
+						spawnMsg.write<MessageType>(MessageType::PlayerSpawn);
+						spawnMsg.write<int>(i);
+						spawnMsg.write<float>(players[i].x);
+						spawnMsg.write<float>(players[i].y);
 
-					serverBroadcast(destroyMsg);
-					destroyMsg.free();
-					break;
+						serverSendTo(spawnMsg, event.userId);
+						spawnMsg.free();
+					}
+					{
+						NetMessage nameMsg;
+						nameMsg.write<MessageType>(MessageType::PlayerName);
+						nameMsg.write<int>(i);
+						nameMsg.write<unsigned char>(strlen(players[i].name));
+						nameMsg.write(players[i].name, strlen(players[i].name));
+
+						serverSendTo(nameMsg, event.userId);
+						nameMsg.free();
+					}
 				}
 
-				case NetEventType::Message:
 				{
-					handleMessage(event.userId, event.message);
-					break;
+					Player* player = &players[event.userId];
+					player->spawn(event.userId, rand() % 800, rand() % 600);
+
+					NetMessage msg;
+					msg.write<MessageType>(MessageType::PlayerSpawn);
+					msg.write<int>(event.userId);
+					msg.write<float>(player->x);
+					msg.write<float>(player->y);
+
+					serverBroadcast(msg);
+					msg.free();
 				}
+
+				{
+					NetMessage msg;
+					msg.write<MessageType>(MessageType::PlayerPossess);
+					msg.write<int>(event.userId);
+
+					serverSendTo(msg, event.userId);
+					msg.free();
+				}
+				break;
+			}
+
+			case NetEventType::UserDisconnected:
+			{
+				engPrint("User %d disconnected", event.userId);
+				players[event.userId].destroy();
+
+				NetMessage destroyMsg;
+				destroyMsg.write<MessageType>(MessageType::PlayerDestroy);
+				destroyMsg.write<int>(event.userId);
+
+				serverBroadcast(destroyMsg);
+				destroyMsg.free();
+				break;
+			}
+
+			case NetEventType::Message:
+			{
+				handleMessage(event.userId, event.message);
+				break;
+			}
 			}
 
 			event.free();
@@ -255,45 +256,30 @@ int WinMain(HINSTANCE, HINSTANCE, char*, int)
 		engText(400, 12, gameStarted ? "STARTED" : "WAITING...");
 
 		// Check if we have a winner
-		if(gameStarted)
+		if (gameStarted)
 		{
-			int numAlivePlayersTeam1 = 0;
-			int numAlivePlayersTeam2 = 0;
+			int numAlivePlayers = 0;
 			int lastAlivePlayer = -1;
 			for (auto& player : players)
 			{
 				if (player.alive)
 				{
-					if (player.teamId == 0)
-					{
-						numAlivePlayersTeam1++;
-					}
-					else if (player.teamId == 1)
-					{
-						numAlivePlayersTeam2++;
-					}
-
-					//++numAlivePlayers;
-					//lastAlivePlayer = player.id;
+					++numAlivePlayers;
+					lastAlivePlayer = player.id;
 				}
 			}
 
-			if (numAlivePlayersTeam1 == 0)
-				engTextf(400, 300, "TEAM RED WINS!");
-			else if(numAlivePlayersTeam2 == 0)
-				engTextf(400, 300, "TEAM GREEN WINS!");
-			/*
+			if (numAlivePlayers == 1)
+				engTextf(400, 300, "'%s' WINS!", players[lastAlivePlayer].name);
 			else if (numAlivePlayers == 0)
 				engText(400, 300, "Draw :(");
-				*/
 		}
-		
+
 
 		for (auto& player : players)
 		{
-			if (player.alive) {
+			if (player.alive)
 				player.update();
-			}
 		}
 
 		for (auto& projectile : projectiles)
